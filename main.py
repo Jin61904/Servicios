e935367fbf08575e1e1496c8e8711744de9f1319
aura73 <laurag.ruizr@konradlorenz.edu.cop>
2023-11-11 09:58:23 -0500

    Colas

---
+++ main.py
@@ -0,0 +1,135 @@
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+import mook
+
+import boto3
+
+aws_access_key_id = 'AKIAXII2BKO7PPH2CJMQ'
+aws_secret_access_key = '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'
+
+sqs = boto3.client(
+    'sqs',
+    region_name='us-east-1',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
+)
+queue_url = 'https://sqs.us-east-1.amazonaws.com/498807690174/banco2'
+
+class Numbers(BaseModel):
+    nums: List[int]
+
+class Nodo():
+    def __init__(self, valor):
+        self.valor = valor
+        self.siguiente = None
+
+    def __repr__(self) -> str:
+        return f"<nodo {self.valor}>"
+
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return{
+        "Servicio": "Estructuras de Datos"
+    }
+
+@app.post("/indices-invertidos")
+def indices_invertidos(palabra: dict):
+    cache = {}
+    for index, documento in enumerate(mook.my_document):
+        words = documento.lower().split()
+
+        for word in words:
+            if word in cache:
+                cache[word].append(mook.my_document[index])
+            else:
+                cache[word] = [mook.my_document[index]]
+    return cache.get(palabra ["palabra"], "No se encontró")
+
+
+
+def detectar_ciclo(lista):
+    tortuga = lista
+    liebre = lista
+
+    while liebre and liebre.siguiente and tortuga:
+        tortuga = tortuga.siguiente
+        liebre = liebre.siguiente.siguiente
+
+        if tortuga == liebre:
+            return tortuga
+
+    return None
+
+@app.get("/floyd-algorithm")
+def floyd_algorithm():
+
+    nodo1 = Nodo(1)
+    nodo2 = Nodo(2)
+    nodo3 = Nodo(3)
+    nodo4 = Nodo(4)
+    nodo5 = Nodo(5)
+
+    nodo1.siguiente = nodo2
+    nodo2.siguiente = nodo3
+    nodo3.siguiente = nodo4
+    nodo4.siguiente = nodo5
+    nodo5.siguiente = nodo2
+
+    nodo_ciclo = detectar_ciclo(nodo1)
+    if nodo_ciclo:
+        return {"tiene_ciclo": True, "valor_nodo_ciclo": nodo_ciclo.valor, "nodo_ciclo": str(nodo_ciclo)}
+    else:
+        return {"tiene_ciclo": False}
+
+
+
+@app.post("/Números-duplicados")
+def find_duplicate(nums: Numbers):
+    nums = nums.nums
+    tortoise = hare = nums[0]
+    while True:
+        tortoise = nums[tortoise]
+        hare = nums[nums[hare]]
+        if tortoise == hare:
+            break
+
+    hare = nums[0]
+    while tortoise != hare:
+        tortoise = nums[tortoise]
+        hare = nums[hare]
+
+    return {"duplicate": hare}
+
+
+@app.get("/FIFO")
+def procces():
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        AttributeNames=[
+            'All'
+        ],
+        MessageAttributeNames=[
+            'All'
+        ],
+        MaxNumberOfMessages=10,  # Ajusta este valor según tus necesidades
+        VisibilityTimeout=30,
+        WaitTimeSeconds=0
+    )
+
+    print(response)
+    if response.get('Messages'):
+        messages = response['Messages']
+        for message in messages:
+            print(f"Mensaje recibido: {message['ReceiptHandle']}")
+            sqs.delete_message(
+                QueueUrl=queue_url,
+                ReceiptHandle=message['ReceiptHandle']
+            )
+    else:
+        print("No se encontraron mensajes en la cola.")
+
+    sqs.close()
